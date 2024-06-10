970d1920d20740c35e0c03535340af376fabed6c
JoelEspinal <j.espinalcarrasco@gmail.com>
2024-06-09 22:05:18 -0400

    Set toolbar visible

--- BB Quotes/BB Quotes/ContentView.swift
+++ BB Quotes/BB Quotes/ContentView.swift
@@ -11,10 +11,12 @@ struct ContentView: View {
     var body: some View {
         TabView {
             Text("Breaking Bad View")
+                .toolbarBackground(.visible, for: .tabBar)
                 .tabItem {
                     Label("Breaking Bad", systemImage: "tortoise")
                 }
             Text("Better Call Saul View")
+                .toolbarBackground(.visible, for: .tabBar)
                 .tabItem {
                     Label("Better Call Saul", systemImage: "briefcase")
                 }
