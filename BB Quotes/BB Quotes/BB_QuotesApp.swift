31bb5f30738b311da030d53a59f0011662b3c5fa
JoelEspinal <j.espinalcarrasco@gmail.com>
2024-06-09 21:41:33 -0400

    Create preject

---
+++ BB Quotes/BB Quotes/BB_QuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BB_QuotesApp.swift
+//  BB Quotes
+//
+//  Created by Joel Espinal on 9/6/24.
+//
+
+import SwiftUI
+
+@main
+struct BB_QuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
