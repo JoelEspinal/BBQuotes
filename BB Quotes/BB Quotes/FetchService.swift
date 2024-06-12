e1b60287e5ed1481716716a8889240c40f8ae740
JoelEspinal <j.espinalcarrasco@gmail.com>
2024-06-12 19:36:40 -0400

    handle response from url

--- BB Quotes/BB Quotes/FetchService.swift
+++ BB Quotes/BB Quotes/FetchService.swift
@@ -16,9 +16,16 @@ struct FetchService {
     let baseURL = URL(string: "https://breaking-bad-api-six.vercel.app/api")!
     
     func fetchQoute(from show: String) async throws -> Quote {
+        // Build fecth url
         let quoteURL = baseURL.appending(path: "quote/random")
         let fetchURL = quoteURL.appending(queryItems: [URLQueryItem(name: "production", value: show)])
         
+        // Fetch data
+        let (data, response) = try await URLSession.shared.data(from: fetchURL)
         
-    }
+        // Handle response
+        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+            throw FetchError.badResponse
+            }
+        }
 }
