9c1d0404485f5355e0417d3df0bf9ac11c1fd6fd
JoelEspinal <j.espinalcarrasco@gmail.com>
2024-06-13 21:44:35 -0400

    Center image as background depending witch show is selected

---
+++ BB Quotes/BB Quotes/QuoteView.swift
@@ -0,0 +1,32 @@
+//
+//  QuoteView.swift
+//  BB Quotes
+//
+//  Created by Joel Espinal on 13/6/24.
+//
+
+import SwiftUI
+
+struct QuoteView: View {
+    let viewModel = ViewModel()
+    let show: String
+    
+    var body: some View {
+        GeometryReader { geo in
+            ZStack {
+                Image(show.lowercased()
+                    .replacingOccurrences(of: " ", with: ""))
+                .resizable()
+                .frame(width: geo.size.width * 2.7, height: geo.size.height * 1.2)
+            }
+            .frame(width: geo.size.width, height: geo.size.height)
+        }
+        .ignoresSafeArea()
+    }
+       
+}
+
+#Preview {
+    QuoteView(show: "Better Call Saul")
+        .preferredColorScheme(/*@START_MENU_TOKEN@*/.dark/*@END_MENU_TOKEN@*/)
+}
--- BB Quotes/BB Quotes/ViewModel.swift
+++ BB Quotes/BB Quotes/ViewModel.swift
@@ -13,8 +13,8 @@ class ViewModel {
     enum FetchStatus {
         case notStarted
         case fetching
-        case sucess
-        case error(error: Error)
+        case success
+        case failed(error: Error)
     }
     
     private(set) var status: FetchStatus = .notStarted
@@ -32,5 +32,19 @@ class ViewModel {
         
         let characterData = try! Data(contentsOf: Bundle.main.url(forResource: "samplecharacter", withExtension: "json")!)
         character = try! decoder.decode(Character.self, from: characterData)
+    }
+    
+    func getData(for show: String) async {
+        status = .fetching
+        
+        do {
+            quote = try await fetcher.fetchQoute(from: show)
+            character = try await fetcher.fetchCharacter(quote.character)
+            character.death = try await fetcher.fetchDeath(for: character.name)
+            
+            status = .success
+        } catch {
+            status = .failed(error: error)
+        }
     }
 }
