f11fa5152774db0740b7ed314ab9d96a8a7d2c64
JoelEspinal <j.espinalcarrasco@gmail.com>
2024-06-25 22:58:43 -0400

    Create character viwe

---
+++ BB Quotes/BB Quotes/CharacterView.swift
@@ -0,0 +1,22 @@
+//
+//  CharacterView.swift
+//  BB Quotes
+//
+//  Created by Joel Espinal on 25/6/24.
+//
+
+import SwiftUI
+
+struct CharacterView: View {
+    
+    let character: Character
+    let show: String
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CharacterView( character: <#T##Character#>, show: "Breaking Bad")
+}
