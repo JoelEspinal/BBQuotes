bdf9ec5d7bb35df3ec8f4879ca8f2cc8715ea723
JoelEspinal <j.espinalcarrasco@gmail.com>
2024-06-27 19:36:40 -0400

    Place background and character images

--- BB Quotes/BB Quotes/CharacterView.swift
+++ BB Quotes/BB Quotes/CharacterView.swift
@@ -13,10 +13,28 @@ struct CharacterView: View {
     let show: String
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { geo in
+            ZStack(alignment: .top, content: {
+                Image(show.lowercased().replacingOccurrences(of: " ", with: ""))
+                    .resizable()
+                .scaledToFit()
+                
+                AsyncImage(url: character.images[0]) { image in
+                    image
+                        .resizable()
+                        .scaledToFill()
+                } placeholder: {
+                    ProgressView()
+                }
+                .frame(width: geo.size.width / 1.2, height: geo.size.height / 1.7)
+                .clipShape(.rect(cornerRadius: 25.0))
+            .padding(.top, 60.0)
+            })
+        }
+        .ignoresSafeArea()
     }
 }
 
 #Preview {
-    CharacterView( character: <#T##Character#>, show: "Breaking Bad")
+    CharacterView( character: ViewModel().character, show: "Breaking Bad")
 }
