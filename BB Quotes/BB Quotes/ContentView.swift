3d2d09001327c961578ac47c234765ea64ba2b4f
JoelEspinal <j.espinalcarrasco@gmail.com>
2024-06-29 09:46:04 -0400

    Fix show naming for El Camino

--- BB Quotes/BB Quotes/ContentView.swift
+++ BB Quotes/BB Quotes/ContentView.swift
@@ -22,7 +22,7 @@ struct ContentView: View {
                     Label("Better Call Saul", systemImage: "briefcase")
                 }
             
-            QuoteView(show: "Better Call Saul")
+            QuoteView(show: "El Camino")
                 .toolbarBackground(.visible, for: .tabBar)
                 .tabItem {
                     Label("El Camino", systemImage: "car")
