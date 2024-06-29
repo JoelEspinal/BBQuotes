01c064374350281b4d7c3739106821423c51f169
JoelEspinal <j.espinalcarrasco@gmail.com>
2024-06-28 21:05:55 -0400

    Add App ios 17 init view

---
+++ BB Quotes/BB Quotes/BBQuotes17App.swift
@@ -0,0 +1,16 @@
+//
+//  BBQuotes17App.swift
+//  BB Quotes
+//
+//  Created by Joel Espinal on 28/6/24.
+//
+
+import SwiftUI
+
+struct BBQuotes17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
